c4107a3e56691cfca3399a921d1f260ec0c43b4a
Natali <piratika309@mail.ru>
2023-02-01 20:21:00 +0300

    Квадрат чисел

---
+++ example001/Program.cs
@@ -0,0 +1,20 @@
+﻿// По двум заданным числам проверять является ли один квадратом второго
+
+Console.WriteLine("Введите первое число: ");
+int Number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int Number2 = int.Parse(Console.ReadLine());
+
+if (Number1 == Number2 * Number2)
+{
+     Console.WriteLine(" Первое число является квадратом второго числа ");
+}
+else if (Number2 == Number1 * Number1)
+{
+    Console.WriteLine(" Второе число является квадратом первого числа ");
+}
+else
+{
+    Console.WriteLine(" Данные числа не являются квадратами друг друга ");
+}
