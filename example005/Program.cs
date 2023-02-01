7d9c2b20d06aa340cabe17687d5787e0a0cece00
Natali <piratika309@mail.ru>
2023-02-02 00:50:38 +0300

    Вывод четных чисел до N

---
+++ example005/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать все четные числа от 1 до N
+
+int number1 = 0;
+int number2 = (number1 + 2);
+
+Console.WriteLine(" Введите число  N, до которого будет выполняться вывод четных чисел : ");
+
+int N =  int.Parse(Console.ReadLine());
+
+while (number2 <= N && number2 %2 == 0)
+
+{
+    Console.WriteLine(number2);
+    number2 = (number2 + 2);
+}
+
