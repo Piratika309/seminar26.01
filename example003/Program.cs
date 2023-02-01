51eb88c788fb71ae81fa2c1455502ee479f482b8
Natali <piratika309@mail.ru>
2023-02-01 23:12:21 +0300

    Максимальное число из 3

---
+++ example003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти максимальное из 3 чисел
+
+Console.WriteLine(" Введите первое число : ");
+int Number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(" Введите второе число : ");
+int Number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(" Введите третье число : ");
+int Number3 = int.Parse(Console.ReadLine());
+
+int max = Number1;
+if(Number2>max) max = Number2;
+if(Number3>max) max = Number3;
+
+Console.WriteLine(" Максимальное число : " + max);
