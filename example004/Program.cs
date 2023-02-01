4ac3b97bc2d3b3c8a438e25d86bc0dfbde81bdf9
Natali <piratika309@mail.ru>
2023-02-01 23:30:49 +0300

    Проверка на четность

---
+++ example004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число четным
+
+Console.WriteLine(" Введите число : ");
+int Number = int.Parse(Console.ReadLine());
+
+if(Number % 2 == 0)
+{
+Console.WriteLine("Введенное Вами число " + Number + " является четным ");
+}
+else
+{
+Console.WriteLine(" Введенное Вами число " + Number + " является нечетным ");
+}
+
