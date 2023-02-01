1773a1f7769593868454e05ac140df9a4be811f6
Natali <piratika309@mail.ru>
2023-02-02 01:58:07 +0300

    Вторая цифра трехзначного числа

---
+++ example006/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать последнюю цифру трехзначного числа
+
+
+Console.WriteLine("Введите трехзначное число: ");
+int Number = int.Parse(Console.ReadLine());
+
+int n1 = (Number/100);
+int n2 = (Number/10) - 10 * n1;
+int n3 = (Number-(n1*100)-(n2*10));
+
+Console.WriteLine("Последняя цифра введенного трехзначного числа :  " + n3); 
