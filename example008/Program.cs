b885b62f7feef176102f142c181ce149c9124421
Natali <piratika309@mail.ru>
2023-02-02 12:13:36 +0300

    Наибольшая цифра числа из отрезка

---
+++ example008/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число из отрезка [10,99]. Показать наибольшее цифру числа
+
+int N = new Random().Next(10,99);
+Console.WriteLine("Дано число " + N);
+
+int a = (N/10);
+int b = (N - a*10);
+
+if (a>b)
+{
+    Console.WriteLine("Максимальная цифра числа : " + a);
+}
+if (a<b)
+{
+Console.WriteLine("Максимальная цифра числа: " + b);
+}
