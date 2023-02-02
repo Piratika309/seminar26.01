4e433d23dfbb25caa4894aa8c18cedd73b62d337
Natali <piratika309@mail.ru>
2023-02-02 13:33:59 +0300

    Удаление цифры из числа

---
+++ example009/Program.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру трехзначного числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int Number = int.Parse(Console.ReadLine());
+
+int n1 = (Number/100);
+int n2 = (Number/10) - 10 * n1;
+int n3 = (Number-(n1*100)-(n2*10));
+
+int N = (10*n1 +n3);
+
+Console.WriteLine("Получившееся число  после удаления  второй цифры:  " + N); 
