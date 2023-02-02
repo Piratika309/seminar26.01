f1a5dc50f75f96455ad25a495b852c973cd02116
Natali <piratika309@mail.ru>
2023-02-02 16:37:40 +0300

    Третья цифра числа

---
+++ example011/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти третью цифру числа или сообщить что ее нет 
+
+
+Console.WriteLine("Введите число: ");
+int Number = int.Parse(Console.ReadLine());
+
+int n3 = (Number % 1000) / 100;
+
+if (Number > 99)
+{
+    Console.WriteLine("Третья цифра числа с конца: " + n3);
+}
+else if (Number < 100)
+{
+Console.WriteLine("Третьей цифры числа нет");
+}
