a675ac80b3130c7158ebdad3542400a167cedf66
Natali <piratika309@mail.ru>
2023-02-02 14:10:50 +0300

    Кратность чисел

---
+++ example010/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+
+Console.WriteLine("Введите первое число, на кратность которого будет проверяться второе число: ");
+int Number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число, кратность которому будет проверяться первое число: ");
+int Number2 = int.Parse(Console.ReadLine());
+
+if (Number2 % Number1 == 0)
+{
+     Console.WriteLine("Второе число " + Number2 + " кратно первому " + Number1);
+}
+else
+{
+    Console.WriteLine("Второе число " + Number2 + " не кратно первому " + Number1);
+    Console.WriteLine("Остаток равен: " + (Number2 % Number1));
+}
